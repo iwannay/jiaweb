6eb20e010323758dc459754f8babe5253178ddf9
johnWang <772648576@qq.com>
2018-01-27 16:10:00 +0800

    update

--- route.go
+++ route.go
@@ -178,7 +178,14 @@ func (r *route) allowed(path, reqMethod string) (allow string) {
 			if method == reqMethod || method == "OPTIONS" {
 				continue
 			}
-
+			_, h, _ := r.NodeMap[method].GetValue(path)
+			if h != nil {
+				if len(allow) == 0 {
+					allow = method
+				} else {
+					allow += ", " + method
+				}
+			}
 		}
 	}
 
